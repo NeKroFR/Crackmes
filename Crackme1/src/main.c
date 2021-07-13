e0d66cf0a863f77831077d4507dd1bec60782f16
Nekro <70316327+NeKroFR@users.noreply.github.com>
2021-07-13 20:23:44 +0200

    Update main.c

--- Crackme1/src/main.c
+++ Crackme1/src/main.c
@@ -36,8 +36,8 @@ int main()
     printf("╚██████╗██║  ██║██║  ██║╚██████╗██║  ██╗██║ ╚═╝ ██║███████╗    ╚██╔═██╔╝ ██║\n");
     printf(" ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═╝ ╚═╝  ╚═╝\n\n");
     
-    char password = "azerty";
-    char input = " ";
+    char password[] = "azerty";
+    char input[250] = " ";
     printf("\033[0;35mEnter the password please:\033[0;37m ");
     scanf("%s",&input);
     printf("\n\n");
