f3bb37b638436f24fb389b9203547a8d4f3717a5
Nekro <70316327+NeKroFR@users.noreply.github.com>
2021-07-14 13:43:52 +0200

    Update main.c

--- Crackme1/src/main.c
+++ Crackme1/src/main.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 #include<string.h>
+#include <conio.h>
 
 
 int main()
 {
-    printf("\033[0;32m                 _.--""""''-.\n");
-    printf("              .-'            '.\n");
-    printf("            .'                 '.\n");
-    printf("           /            .        )\n");
-    printf("          |                   _  (\n");
-    printf("          |          .       / \\  \\\n");
-    printf("          \\         .     .  \\_/  |\n");
-    printf("           \\    .--' .  '         /\n");
-    printf("            \\  /  .'____ _       /,\n");
-    printf("             '/   (\\    `)\\       |\n");
-    printf("             ||\\__||    |;-.-.-,-,|\n");
-    printf("             \\\\___//|   \\--'-'-'-'|\n");
-    printf("              '---' \\             |\n");
-    printf("       .--.          '---------.__)   .-.\n");
-    printf("      .'   \\     created by         /  '.\n");
-    printf("     (      '.                    _.'     )\n");
-    printf("      '---.   '.    NEKRO     _.-'    .--'\n");
-    printf("           `.   `-._      _.-'   _.-'`\n");
-    printf("             `-._   '-.,-'   _.-'\n");
-    printf("                 `-._   `'.-'\n");
-    printf("               _.-'` `;.   '-._\n");
-    printf("        .--.-'`  _.-'`  `'-._  `'-.--.\n");
-    printf("       (       .'            '.       )\n");
-    printf("        `,  _.'                '._  ,'\n");
-    printf("          ``                      ``\n");
+
+
+
+    printf("\033[0;32m                       ______\n");
+    printf("                    .-\"      \"-.\n");
+    printf("                   /            \\\n");
+    printf("       _          |              |  \n");
+    printf("      ( \         |,  .-.  .-.  ,|         / )\n");
+    printf("       > \"=._     | )(__/  \__)( |     _.=\" <\n");
+    printf("      (_/\"=._\"=._ |/     /\     \| _.=\"_.=\"\_)\n");
+    printf("             \"=._ (_     ^^     _)\"_.=\"\n");
+    printf("                _.=\"| \IIIIII/ |\"=._\n");
+    printf("                _.=\"| \IIIIII/ |\"=._\n");
+    printf("      _     _.=\"_.=\"\\          /\"=._\"=._     _\n");
+    printf("     ( \\_.=\"_.=\"     `--------`     \"=._\"=._/ )\n");
+    printf("      > _.=\"                            \"=._ <\n");
+    printf("     (_/          \033[0;37m   \033[46mcreated by nekro\033[0;32m      \_)\n");
     printf(" ██████╗██████╗  █████╗  ██████╗██╗  ██╗███╗   ███╗███████╗     ██╗ ██╗  ██╗\n");
     printf("██╔════╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝████╗ ████║██╔════╝    ████████╗███║\n");
     printf("██║     ██████╔╝███████║██║     █████╔╝ ██╔████╔██║█████╗      ╚██╔═██╔╝╚██║\n");
@@ -38,19 +31,19 @@ int main()
     
     char password[] = "*****";
     char input[250] = " ";
-    printf("\033[0;35mEnter the password please:\033[0;37m ");
+    printf("\033[0;35mEnter the password please:\033[0m ");
     scanf("%s",&input);
     printf("\n\n");
     
     if (strcmp(input,password) == 0)
     {
-        printf("\033[0;36mWell done !");
+        printf("\033[0;36mWell done !\n");
     }
     else
     {
-        printf("\033[0;31mThis is not the good password !");
+        printf("\033[0;31mThis is not the good password !\n");
     }
-    
+    getch();
     
     return 0;
 }
