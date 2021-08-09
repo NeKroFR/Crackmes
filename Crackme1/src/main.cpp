4b9a094ce2121459c8325e48d9308032980ecea9
Nekro <70316327+NeKroFR@users.noreply.github.com>
2021-08-09 19:03:52 +0200

    merge to c++
    i can't choose a language xD

---
+++ Crackme1/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  str input = "";
+  str password = "";
+  std::cout << "Enter the password please: ";
+  cin >> inputs;
+  
+  if (strcmp (key,buffer) != 0)
+    {
+        cout << "Well done !\n";  
+    }
+    else
+    {
+        cout << "his is not the good password !\nTry Again !";
+    }
+      
+    return 0;
+}
